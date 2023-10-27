8944836a9085b074319c425c41e02e71a3232775
gm-machine <username@users.noreply.github.com>
2023-10-27 08:57:39 +0000

    :bookmark: 1.0.1 [ci skip]

--- Package.swift
+++ Package.swift
@@ -1,21 +1,20 @@
-// swift-tools-version:5.7
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "nunav-design-system",
-    defaultLocalization: "en",
-    platforms: [.iOS(.v13)],
+    name: "nunav-ios-design-system",
     products: [
         .library(
             name: "NunavDesignSystem",
-            targets: ["NunavDesignSystem"]
-        )
+            targets: ["NunavDesignSystem"]),
     ],
     targets: [
-        .target(
-            name: "NunavDesignSystem"
+        .binaryTarget(
+            name: "NunavDesignSystem",
+            url: "https://www.github.com/Graphmasters/nunav-ios-design-system-distribution/releases/download/1.0.1/NunavDesignSystem.xcframework.zip",
+            checksum: "ed961e5cac7040ad8afed78639dc22111c5a701b8b9e7f27bb18dc402dedaeb7"
         )
     ]
 )
