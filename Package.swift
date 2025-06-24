5094bfc45eed57cf36c85c6f15e3e8254119aaa8
gm-machine <username@users.noreply.github.com>
2025-06-24 12:53:59 +0000

    :bookmark: 1.0.38 [ci skip]

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NunavDesignSystem",
-            url: "https://www.github.com/Graphmasters/nunav-ios-design-system-distribution/releases/download/1.0.37/NunavDesignSystem.xcframework.zip",
-            checksum: "476cac4e7877d4f36a52b2fef946adeb46a6643a15b8d18aa7b84a774283039c"
+            url: "https://www.github.com/Graphmasters/nunav-ios-design-system-distribution/releases/download/1.0.38/NunavDesignSystem.xcframework.zip",
+            checksum: "d1325999f9080f577cece2d13110a5cd1199e347684b14adc19b9725c60d51fa"
         )
     ]
 )
